ce796e02bed40020f5cbfcba26497f2853760f6e
Siddhanth <siddhanthgumber123@gmail.com>
2025-06-25 10:00:40 +0530

    Bug fount

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -19,6 +19,7 @@ const useAuthStore = create((set)=>({
             set({isCheckingAuth:false})
         }
     },
+
     signup: async (data)=>{
         set({isSigningUp:true});
        try {
@@ -30,6 +31,43 @@ const useAuthStore = create((set)=>({
        } finally{
         set({isSigningUp:false});
        }
-    }
+    },
+
+    login:async(data)=>{
+        set({isLogingIn:true})
+        try {
+            const res=await axiosInstance.post("/auth/login",data)
+            set({authUser:res.data});
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }finally{
+            set({isLogingIn:false});
+        }
+    },
+
+    logout: async()=>{
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser:null})
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error/response.data.message);
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+        const res = await axiosInstance.put("/auth/update-profile", data);
+        set({ authUser: res.data });
+        toast.success("Profile updated successfully");
+        } catch (error) {
+        console.log("error in update profile:", error);
+        toast.error(error.response.data.message);
+        } finally {
+        set({ isUpdatingProfile: false });
+        }
+    },
 }))
 export default useAuthStore;
